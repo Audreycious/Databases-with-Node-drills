67b52b9dc2befca206e34e723d7b0ca6ec46a037
Audreycious <porcupine4@gmail.com>
2019-09-09 00:23:49 -0500

    done with blogful

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+INSERT INTO blogful_articles
+    (title, content, date_published)
+VALUES
+    ('First test post!',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?', '09/08/2019'),
+    ('Second test post!',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.', '09/08/2019'),
+    ('Third test post!',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.', '09/08/2019');
+
+COMMIT;
